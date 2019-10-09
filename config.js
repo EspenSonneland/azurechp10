cc4ec2dac64cdc7dc81df78cf06fe4cc0d5420e0
EspenSonneland <espensonneland@gmail.com>
2019-10-09 13:10:39 +0000

    Pizza

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {}
+
+config.uri = "https://azuremolecp.documents.azure.com:443/";
+config.primaryKey = "If5P4Po9ewBYyXnxCWhDfDwHc5RByToFngMVfsP94F6OiUUIeKJ4BWLp3uwgH0QfoA2P441s4P2fsQDyPAkNbQ==";
+config.database = { "id": "pizzadb" };
+config.collection = { "id": "pizzas" };
+
+module.exports = config;
